f11b8753ee4d17e5421d0cdeea1b62c729968769
Shreyas Sali <ssali@mezeo.com>
2012-03-01 17:18:45 -0600

    Initial Commit

---
+++ Comments/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Comments
+//
+//  Created by Shreyas Sali on 3/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Comments/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Comments
+//
+//  Created by Shreyas Sali on 3/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
